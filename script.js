4cf4fc6f100ecbbcef95f0f7c3451816bfd4102e
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-18 19:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let line = document.getElementById("line")
+let degrees = 1
+setInterval(function() {
+    degrees = degrees + 1
+    line.style.transform = 'rotate(' + degrees + 'deg)'
+}, 10);
